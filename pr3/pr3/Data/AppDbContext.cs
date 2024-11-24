1b79e421b757a2e8c8e337564c50473a3861bccc
Vladolion <vladforschool@gmail.com>
2024-11-24 15:09:38 +0200

    pr3
    never again. 0/10

---
+++ pr3/pr3/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using pr3.Models;
+
+
+namespace pr3.Data
+{
+	public class AppDbContext :DbContext
+	{
+		public AppDbContext( DbContextOptions<AppDbContext> options ) : base( options ) { }
+
+		public DbSet<Product> Products { get; set; }
+	}
+}
