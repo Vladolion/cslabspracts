993af03ff4d959ade1b552c72d4f4585b45a9ced
Vladolion <vladforschool@gmail.com>
2024-12-14 12:56:08 +0200

    pr4

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+class Student
+{
+	[JsonInclude]
+	public string name = "";
+
+	[JsonInclude]
+	public int age = 0;
+
+	[JsonInclude]
+	public List<string> subjects = new List<string>();
+
+
+	public Student() { }
+	public Student( string NAME, int AGE, List<string> SUBJECTS )
+	{
+		name = NAME;
+		age = AGE;
+		subjects = SUBJECTS;
+	}
+}
+class pr4
+{
+	public static void Main()
+	{
+		string fileName = "students.json";
+		List<Student> studentsForJson = new List<Student>();
+		studentsForJson.Add( new Student( "Adolf Gaga", 32, new List<string> { "Arts", "Hello world" } ) );
+		studentsForJson.Add( new Student( "Adolf Gaga", 32, new List<string> { "Arts", "Hello world" } ) );
+		studentsForJson.Add( new Student( "Adolf Gaga", 32, new List<string> { "Arts", "Hello world" } ) );
+
+		string jsonFromStudents = JsonSerializer.Serialize( studentsForJson );
+
+		File.WriteAllText( fileName, jsonFromStudents );
+
+
+		string jsonFromFile = File.ReadAllText( fileName );
+
+		List<Student> studentsFromJson = JsonSerializer.Deserialize<List<Student>>( jsonFromFile );
+
+		foreach ( Student i in studentsFromJson )
+		{
+			Console.WriteLine( i.name );
+		}
+	}
+}
